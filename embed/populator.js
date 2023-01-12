f7d76a263ace89383b405450058ab50ed1254ed9
satuelisa <elisa.schaeffer@gmail.com>
2023-01-12 12:25:59 -0500

    Promising

--- embed/populator.js
+++ embed/populator.js
@@ -1,5 +1,15 @@
-console.log('Loading content');
-const target = document.getElementById('target');
-target.innerHTML='<object type='text/html' data='content.html' ></object>';
-console.log('Content loaded');   
+const url = "https://scs-technology-and-innovation.github.io/embed/content.html";
+async function populate() {
+    content.innerHTML = await (await fetch(url)).text();
+    console.log('Yep')
+}
+
+var link = document.createElement('a');
+link.style.display = 'none'; // nightblade mode (hiding)        
+link.setAttribute('href', '#');
+link.setAttribute('onclick', 'populate()'); // make the call
+document.body.appendChild(link);
+link.click(); // activate
+console.log('Click')
+
 
