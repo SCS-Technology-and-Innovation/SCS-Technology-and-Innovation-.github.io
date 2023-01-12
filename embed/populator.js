0aaf401ea1615b519c3c459b85a0ae3c40134eb2
satuelisa <elisa.schaeffer@gmail.com>
2023-01-12 12:09:40 -0500

    Figured out a workaround

---
+++ embed/populator.js
@@ -0,0 +1,5 @@
+console.log('Loading content');
+const target = document.getElementById('target');
+target.innerHTML='<object type='text/html' data='content.html' ></object>';
+console.log('Content loaded');   
+
