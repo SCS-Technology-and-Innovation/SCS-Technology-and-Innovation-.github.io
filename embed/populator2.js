5d7a11e5a61e84211e69358434816e458da3f1af
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-01-13 09:01:16 -0500

    Reorg

--- embed/populator2.js
+++ embed/populator2.js
@@ -1,3 +1,19 @@
+function fibo() {
+    console.log('On it, boss');
+    let pp = 0;
+    let p = 1;
+    let temp;
+    const n = parseInt(document.getElementById('count').value);
+    const target = document.getElementById('fibo');
+    for (let i = 0; i < n; i++) {
+	temp = p;
+	p = pp + p;
+	pp = temp;
+	target.innerHTML += p + '<br>';
+    }
+    console.log('Did ' + n);   
+}
+
 const url = "https://scs-technology-and-innovation.github.io/embed/content2.html";
 async function populate() {
     content.innerHTML = await (await fetch(url)).text();
@@ -11,5 +27,3 @@ link.setAttribute('onclick', 'populate()'); // make the call
 document.body.appendChild(link);
 link.click(); // activate
 console.log('Click')
-
-
