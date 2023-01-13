7347685cce6039c82a3fed19bef31c8f88d2fba0
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-01-13 08:50:16 -0500

    Nesting testing

---
+++ embed/populator2.js
@@ -0,0 +1,15 @@
+const url = "https://scs-technology-and-innovation.github.io/embed/content2.html";
+async function populate() {
+    content.innerHTML = await (await fetch(url)).text();
+    console.log('Yep')
+}
+
+var link = document.createElement('a');
+link.style.display = 'none'; // nightblade mode (hiding)        
+link.setAttribute('href', '#');
+link.setAttribute('onclick', 'populate()'); // make the call
+document.body.appendChild(link);
+link.click(); // activate
+console.log('Click')
+
+
