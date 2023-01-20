68fc9cb0bbc7dfaec39286700e01897b8f36014a
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-01-20 12:23:48 -0500

    Static embeds do NOT update

---
+++ embed/populator3.js
@@ -0,0 +1,29 @@
+function fibo() {
+    console.log('On it, boss');
+    let pp = 0;
+    let p = 1;
+    let temp;
+    const n = parseInt(document.getElementById('count').value);
+    const target = document.getElementById('fibo');
+    for (let i = 0; i < n; i++) {
+	temp = p;
+	p = pp + p;
+	pp = temp;
+	target.innerHTML += p + '<br>';
+    }
+    console.log('Did ' + n);   
+}
+
+const url = "https://scs-technology-and-innovation.github.io/embed/static.html";
+async function populate() {
+    content.innerHTML = await (await fetch(url)).text();
+    console.log('Yep')
+}
+
+var link = document.createElement('a');
+link.style.display = 'none'; // nightblade mode (hiding)        
+link.setAttribute('href', '#');
+link.setAttribute('onclick', 'populate()'); // make the call
+document.body.appendChild(link);
+link.click(); // activate
+console.log('Click')
