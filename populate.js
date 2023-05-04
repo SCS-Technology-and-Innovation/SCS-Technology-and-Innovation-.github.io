925120f82223ce35668097a2668bf32b4ad579ca
satuelisa <elisa.schaeffer@gmail.com>
2023-05-04 13:12:02 -0400

    Missing line

--- populate.js
+++ populate.js
@@ -2,6 +2,7 @@ async function grab() {
     console.log('request:', course, module, element);
     const url = "https://scs-technology-and-innovation.github.io/Content/CCCS" + course + '/M' + module + '/' + element + '.html';
     console.log(url);
+    const content = document.getElementById("content");
     content.innerHTML = await (await fetch(url)).text();
 }
 var link = document.createElement('a');
