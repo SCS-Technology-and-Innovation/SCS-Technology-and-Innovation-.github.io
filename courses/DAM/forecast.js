2c0d7ce03a6558159f064869fee504e3758628eb
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-06-22 09:05:50 -0400

    Some fixed and weird hacks for the trend

--- courses/DAM/forecast.js
+++ courses/DAM/forecast.js
@@ -5,16 +5,13 @@ function normal(m, s) {
 
 let raw = [];
 let data = [];
-let errors = [ null ];
-let forecast = [ null ];
-
-let low = null;
-let high = null;
-
+let errors = [ ];
+let forecast = [ ];
 var canvas = document.getElementById('timeseries');
 var ctx = canvas.getContext('2d');
 
-let margin = 30;
+const margin = 30;
+const capacity = 50;
 
 function scale(v, vmin, vmax, pixels, flip) {
     let vspan = vmax - vmin;
@@ -48,6 +45,7 @@ function regression(yv, xv) {
 	console.log('Array length mismatch in regression:', xv, yv);
 	n = Math.min(xv.length, yv.length);
     }
+    // console.log(n + '-point regression with', yv, xv);
     let sx = 0;
     let sy = 0;
     let xsq = 0;
@@ -80,46 +78,52 @@ function arima() { // as simplified as possible, nothing fancy
 	v = diff(v);
     }
     ac(v);
-    if (v.length >= p) { // able to do both AR
+    let last = v.length;
+    if (p > 1 && last > p) { // able to do AR
 	let pos = [];
-	for (let i = v.length - p; i < v.length; i++) {
+	for (let i = last - p; i < last; i++) {
 	    pos.push(i);
 	}
-	let AR = regression(v.slice(v.length - pos.length), pos);
-	let arpart = AR[0] * v.length + AR[1]; // the AR part
-	// console.log('AR gave', arpart);
+	let AR = regression(v.slice(last - p), pos);
+	let arpart = AR[0] * last + AR[1]; // the AR part
+	// console.log('AR gave', arpart, AR);
 	f += arpart;
-	if (errors.length >= q && count >= q) { // able to do the MA part
-	    pos = [];
-	    for (let i = errors.length - q; i < errors.length; i++) {
-		pos.push(i - 1);
-	    }	    
-	    let MA = regression(errors.slice(errors.length - pos.length), pos);
-	    mapart = (MA[0] * errors.length + MA[1]); // include the MA part
-	    // console.log('MA gave', mapart);	    
-	    f -= ew * mapart;
+    } else if (p == 1) {
+	f = v[last - 1]; // use the last difference
+    }
+    last = errors.length;
+    if (q > 1 && last >= q && count >= q) { // able to do the MA part
+	pos = [];
+	for (let i = last - q; i < last; i++) {
+	    pos.push(i - 1);
 	}
+	// errors are positive if the forecast has been too small the whole time
+	// so the regression will give a positive number 
+	let MA = regression(errors.slice(last - pos.length), pos);
+	mapart = (MA[0] * last + MA[1]); // include the MA part
+	// console.log('MA gave', mapart, MA);
+	// we add the regression result to the new forecast
+	f += ew * mapart;
+    } else if (q == 1 && errors[last - 1] != null) {
+	f += ew * errors[last - 1]; // adjust to the previous error
     }
     if (d == 1) { // that was a difference and not the value itself
 	let prev = data[k - 1];
 	// console.log('undiff from', prev);
 	f += data[k - 1];
     }
-    if (f < low) {
-	low = f;
-    }
-    if (f > high) {
-	high = f;
-    }
     forecast.push(f); // store the forecast
     // console.log(data[k - 1], 'versus', f);
 }
+
+const maxac = 5;
 
 function ac(values) {
     let n = values.length;
     let threshold = parseFloat(document.getElementById("thr").value);
     target.innerHTML = ''; // reset
     let pending = true;
+    let printouts = 0;
     for (let l = 1; l <= n / 2; l++) { // lag
 	let xm = 0;
 	let ym = 0;
@@ -142,6 +146,11 @@ function ac(values) {
 	let c = xy / Math.sqrt(xd * yd);
 	if (c >= threshold) {
 	    target.innerHTML += 'Lag ' + l + ' has an autocorrelation of ' + c.toFixed(2) + '<br>';
+	    printouts++;
+	    if (printouts >= maxac) { // too many
+		target.innerHTML += 'Autocorrelation report truncated at ' + (l - 1);
+		return; // only the initial ones interest us		
+	    }
 	    pending = false;
 	} else if (!pending) {
 	    target.innerHTML += 'Autocorrelation falls below threshold after lag ' + (l - 1);
@@ -155,10 +164,30 @@ function draw() {
     let px = null;
     let py = null;
     let n = data.length;
-    for (let i = 0; i < n; i++) {
+    let start = n - capacity; // show the tail
+    if (start < 0) {
+	start = 0;
+    }
+    let low = null;
+    let high = null;
+    for (let i = start; i < n; i++) {
+	if (low == null || data[i] < low) {
+	    low = data[i];
+	}
+	if (low == null || (forecast[i] != null && forecast[i] < low)) {
+	    low = forecast[i];
+	}
+	if (high == null || data[i] > high) {
+	    high = data[i];
+	}
+	if (high == null || (forecast[i] != null && forecast[i] > high)) {
+	    high = forecast[i];
+	}
+    }
+    for (let i = start; i < n; i++) {
 	// draw the data
 	ctx.lineWidth = 2;
-	let x = scale(i, 0, n, w, false);
+	let x = scale(i, start, n, w, false);
 	let y = scale(data[i], low, high, h, true);
 	if (px != null) {
 	    ctx.strokeStyle = 'green';
@@ -171,56 +200,77 @@ function draw() {
 	py = y;
 	// draw the forecast
 	if (forecast[i] != null) {
-	    x = scale(i, 0, n, w, false); 
+	    x = scale(i + 1, start, n, w, false); 
 	    y = scale(forecast[i], low, high, h, true);
 	    ctx.lineWidth = 1;
 	    ctx.strokeStyle = 'orange';
+	    ctx.fillStyle = 'yellow';
 	    ctx.beginPath();
 	    ctx.arc(x, y, 3, 0, 2 * Math.PI);
 	    ctx.stroke();
+	    ctx.fill();
 	}
     }
 }
 
 document.addEventListener('keypress', generate);
 
+let level = 0;
+let change = 0;
+let t = parseFloat(document.getElementById("trend").value); 
+
+function setlevel() {
+    let newt = parseFloat(document.getElementById("trend").value);
+    if (newt != t) {
+	let i = data.length;
+	if (i > 0) {
+	    level = data[i - 1]; 
+	    change = i;
+	}
+	if (newt * t == -1) { // opposite signs
+	    level *= -1;
+	} else if (newt > t) {
+	    level /= 2; // a smoothness hack
+	}
+	t = newt;
+	console.log('Level now at', level);
+    }
+}
+
 function generate() {
     let p = null;
     if (document.getElementById("season").checked) {
 	p = parseFloat(document.getElementById("period").value);
     }
-    let t = parseFloat(document.getElementById("trend").value); // dy / dx
-    let m = parseFloat(document.getElementById("mean").value);
-    let s = parseFloat(document.getElementById("sd").value);
-
     let i = data.length;
-    let r = null;
-     if (p != null) { // season
+    let r = 0;
+    if (p != null) { // season
 	if (i > p) {
 	    let old = raw[i - p];
-	    r = old; // oversimplification
+	    r = old; // as simple as possible: repeat history
 	}
-     }
+    }
+    let m = parseFloat(document.getElementById("mean").value);
+    let s = parseFloat(document.getElementById("sd").value);    
     r += normal(m, s); // add the noise        
     raw.push(r);
-    r += t * i; // add the trend
-    if (low == null || r < low) {
-	low = r;
-    }
-    if (high == null || r > high) {
-	high = r;
-    }
+    r += ((i - change) * t) + level; // add the trend
     data.push(r);
+    // now, forecast the next point
+    arima();
     let n = forecast.length;
+    // determine the error
     let latest = forecast[n - 1];
     if (latest != null) {
-	let e = r - latest;
-	// console.log('Error', e);
+	let e = r - latest; // positive if the forecast is too small
 	errors.push(e);
 	count++;
     } else {
 	errors.push(null);
+	if (count > 0) {
+	    console.log('Forecasting interruption');
+	    count = 0;
+	}
     }	
-    arima();
     draw();
 }
