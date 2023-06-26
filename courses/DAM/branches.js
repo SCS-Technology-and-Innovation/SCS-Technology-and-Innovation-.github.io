8d5f51d3b5ade816f75ff7f0db3e678702bdd274
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-06-26 12:31:21 -0400

    Stats and better colors

--- courses/DAM/branches.js
+++ courses/DAM/branches.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("tree");
 const ctx = canvas.getContext("2d");
-const SIZE = 5; // node radius
+const SIZE = 10; // node radius
+const XOFFSET = -5;
+const YOFFSET = 3;
 
 // factorial
 function fact(n) {
@@ -9,7 +11,7 @@ function fact(n) {
     } else {
 	return n * fact(n - 1);
     }
-}    
+}
 
 // algorithm from  https://www.johndcook.com/blog/2010/06/14/generating-poisson-random-values/
 // tweaked to function with low rates
@@ -45,18 +47,20 @@ function poisson(truerate) {
 
 function branch(n, l, c, m) {
     if (n.depth >= m) {
+	flag = true;
 	return; // max depth
     }
     let k = poisson(l);
     // console.log(l, k);
     let d = n.depth + 1;
     for (let i = 0; i < k; i++) {
-	let newborn = { 'children': [], 'depth': d, 'width': 1 };
-	quota--; // node created
 	if (quota == 0) {
-	    document.getElementById("note").innerHTML =
-		'Maximum total size reached.';
+	    document.getElementById("tnote").innerHTML =
+		'Total size limit enforced.';
+	    break;
 	}
+	let newborn = { 'children': [], 'depth': d, 'width': 1, 'height' : 1 };
+	quota--; // node created
 	n.children.push(newborn);
 	if (quota >= 0) { // room to grow
 	    branch(newborn, l * c, c, m);
@@ -65,17 +69,46 @@ function branch(n, l, c, m) {
 }
 
 let quota = 0;
+let flag = false;
+let root = null;
+
 function grow() {
-    document.getElementById("note").innerHTML = '';
-    let root = { 'height': 1, 'children': [], 'width': 1, 'depth': 0 };
+    document.getElementById("hnote").innerHTML = '';
+    document.getElementById("tnote").innerHTML = '';    
+    root = { 'height': 1, 'children': [], 'width': 1, 'depth': 0 };
     let l = parseFloat(document.getElementById("rate").value);
     let c = parseFloat(document.getElementById("cool").value);
     let m = parseInt(document.getElementById("maxdepth").value);
-    quota = parseInt(document.getElementById("maxsize").value) - 1; // we already have a root node        
+    // we already have a root node            
+    quota = parseInt(document.getElementById("maxsize").value) - 1;
+    flag = false;
     branch(root, l, c, m);
+    show();
+}
+
+function show() {
+    if (flag) {
+	document.getElementById("hnote").innerHTML += 'Height limit enforced.';
+    } else {
+	document.getElementById("hnote").innerHTML += '';
+    }
+    leaves = 0;
+    internal = 0;
     update(root);
+    document.getElementById("height").innerHTML =
+	'Height is ' + root.height + '.';
+    document.getElementById("width").innerHTML =
+	'Total width is ' + root.width + '.';
+    document.getElementById("total").innerHTML =
+	'' + (leaves + internal) + ' nodes.';    
+    document.getElementById("leaves").innerHTML =
+	'' + leaves + ' leaves.';
+    document.getElementById("internal").innerHTML =
+	'' + internal + ' internal (router) nodes.';
     draw(root);
 }
+
+let display = "N";
 
 function node(n) {
     let k = n.children.length;
@@ -83,7 +116,7 @@ function node(n) {
 	node(n.children[i]);
     }
     if (k == 0) { // no children
-	ctx.fillStyle = "#27ae60";
+	ctx.fillStyle = "#CD5C5C";
     } else {
 	ctx.fillStyle = "#5499c7";    
     }
@@ -91,7 +124,23 @@ function node(n) {
     ctx.arc(n.x, n.y, SIZE, 0, 2 * Math.PI);
     ctx.closePath();
     ctx.fill();
-    // ctx.fillText(n.width, n.x + SIZE, n.y + SIZE);
+    ctx.fillStyle = "#ffffff";        
+    switch (display) {
+    case "W":
+	ctx.fillText(n.width, n.x + XOFFSET, n.y + YOFFSET);
+	break;
+    case "H":
+	ctx.fillText(n.height, n.x + XOFFSET, n.y + YOFFSET);
+	break;
+    case "D":
+	ctx.fillText(n.depth, n.x + XOFFSET, n.y + YOFFSET);
+	break;
+    case "C":
+	ctx.fillText(n.children.length, n.x + XOFFSET, n.y + YOFFSET);
+	break;		
+    default:
+	break;
+    } 
 }
 
 function position(n, sx, sy, aw, ah) {
@@ -124,8 +173,10 @@ function connect(n) {
     }
 }
 
+let leaves = 0;
+let internal = 0;
+
 function update(n) {
-    n.height = 0;
     let k = n.children.length;
     if (k > 0) {
 	var h = 0;
@@ -138,6 +189,9 @@ function update(n) {
 	}
 	n.width = t;
 	n.height = h + 1;
+	internal++;
+    } else {
+	leaves++;
     }
 }
 
@@ -147,15 +201,16 @@ function draw(root) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     var tall = root.height;
     var wide = root.width;
-    var MARGIN = 6 * SIZE;
+    var MARGIN = 2 * SIZE;
     var unit = 3 * SIZE;
     var w = unit * wide + 2 * MARGIN;
-    var h = 2 * unit * tall + 2 * MARGIN;
+    var h = 2 * unit * tall + 4 * MARGIN;
     canvas.width = w;
     canvas.height = h;
     ctx.lineWidth = 3;
     // compute coordinates
     position(root, MARGIN, MARGIN / 2, w - 2 * MARGIN, 2 * unit);
     connect(root); // lines
+    display = document.getElementById("label").value;
     node(root); // nodes
 }    
