9892dc9c32b5b418f5888e651455a2607e3b6606
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-07-13 20:31:54 -0400

    Mathieu is out so I accidentally wrote some evening code

--- Tools/eval.py
+++ Tools/eval.py
@@ -11,9 +11,6 @@ def parse(line):
     instructor = ' '.join(line.split('-')[-1].split()[:-2])
     return year, instructor
 
-dataset = {}
-scores = {}
-
 TOTAL = 0
 SDISAGREE = 1
 DISAGREE = 2
@@ -46,108 +43,133 @@ def match(c, s):
         else:
             return AGREE
 
-for filename in os.listdir('.'):
-    if '.pdf' in filename:
-        f = os.path.join('.', filename)
-        if os.path.isfile(f):
-            reader = PdfReader(f)
-            images = fitz.open(f)
-            q = 1
-            scores[filename] = {}
-            for i in range(len(images)):
-                page = images[i]
-                l = page.get_images(full = True)
-                if l:
-                    for ii, img in enumerate(l, start = 1):
-                        xref = img[0]
-                        base_image = images.extract_image(xref)
-                        image_bytes = base_image["image"]
-                        image_ext = base_image["ext"]
-                        image = Image.open(io.BytesIO(image_bytes))
-                        text = pytesseract.image_to_string(image)
-                        if 'tron' in text: # just some match (less is more for OCR)
-                            values = {} # start looking for the counts
-                            text = text.replace('\n', ' ')
-                            check = False
-                            category = None
-                            strongly = False
-                            for w in text.split():
-                                w = w.strip().lstrip()
-                                if len(w) == 0:
-                                    continue
-                                if 'ongl' in w:
-                                    strongly = True
-                                if relevant(w):
-                                    check = True
-                                    category = w
-                                    continue
-                                if check:
-                                    if ')' in w: # count should be there
-                                        p = w.split(')')[0].replace('(', '')
-                                        try:
-                                            v = int(p)
-                                            # print('got', v, 'using', p, 'from', w)
-                                        except:
-                                            print(w, 'is not an integer', file = stderr)
-                                            v = None
-                                        if v is not None:
-                                            values[match(category, strongly)] = v
-                                            strongly = False                                            
-                                            category = None
-                                            check = False                                    
-                            scores[filename][q] = values
-                            # print(q, values)
-                            q += 1
-            content = ''
-            for page in reader.pages:
-                content += page.extract_text()
-            question = False
-            formulation = ''
-            term = None
-            year = None
-            instructor = None
-            lettercode = None
-            numbercode = None
-            section = None
-            prev = None
-            listing = []
-            for line in content.split('\n'):
-                line = line.strip().lstrip()
-                if len(line) == 0:
-                    continue
-                if 'Mean' in line or 'Statistics' in line:
-                    question = False
-                    formulation = formulation.lstrip()
-                    if len(formulation) > 0:
-                        if formulation not in listing:
-                            listing.append(formulation)
-                    formulation = ''
-                    continue
-                if (line[0] == 'Q' and 'Questionnaire' not in line) \
-                   or 'In general' in line \
-                   or (line[:4] == 'The ' and 'ratings' not in line):
-                    question = True
-                if question:
-                    if line not in formulation:
-                        formulation += ' ' + line
-                if 'Mercury Course Evaluation' in line:
-                    continue
-                if '(Fall' in line:
-                    term = 'F'
-                    year, instructor = parse(line)
-                elif '(Winter' in line:
-                    term = 'W'
-                    year, instructor = parse(line)
-                elif '(Summer' in line:
-                    term = 'S'
-                    year, instructor = parse(line)
-                if 'Course Name ' in line:
-                    fields = line.split()
-                    lettercode = fields[2]
-                    numbercode = fields[3][:-1] # colon
-                    section = fields[-3][:-1] # comma
-            dataset[filename] = (term, year, lettercode, numbercode, section, instructor, listing)
+def ocrfix(text):
+    text = text.replace('\n', ' ')                    
+    text = text.replace('[', '(') 
+    text = text.replace(']', ')')
+    text = text.replace('{', '(')
+    text = text.replace('}', ')') 
+    text = text.replace('( ', '(')
+    text = text.replace(' )', ')')
+    text = text.replace('°', '0')
+    return text
 
+def ocrnumber(w):
+    w = w.replace('o', '0')
+    w = w.replace('O', '0')
+    w = w.replace(':', '1')
+    w = w.replace('i', '1')
+    w = w.replace('f', '1')
+    w = w.replace('l', '1')
+    w = w.replace('I', '1')
+    w = w.replace('s', '5')
+    w = w.replace('S', '5')            
+    return w
+
+def diagrams(filename):
+    images = fitz.open(filename)
+    q = 1
+    scores = {}
+    for i in range(len(images)):
+        page = images[i]
+        l = page.get_images(full = True)
+        if l:
+            for ii, img in enumerate(l, start = 1):
+                xref = img[0]
+                base = images.extract_image(xref)
+                b = base["image"]
+                image = Image.open(io.BytesIO(b))
+                text = pytesseract.image_to_string(image)
+                if 'tron' in text: # just some match (less is more for OCR)
+                    values = {} # start looking for the counts
+                    check = False
+                    category = None
+                    strongly = False
+                    text = ocrfix(text)
+                    for w in text.split():
+                        w = w.strip().lstrip()
+                        if len(w) == 0:
+                            continue
+                        if 'ongl' in w:
+                            strongly = True
+                        if relevant(w):
+                            check = True
+                            category = w
+                            continue
+                        if check: # right after a relevant word
+                            check = False # lower the flag                                   
+                            v = None
+                            if '(' in w or ')' in w: # count should be in parenthesis
+                                w = w.replace('(', '') # clean out the parenthesis
+                                w = w.replace(')', '')
+                            try:
+                                v = int(ocrnumber(w))
+                            except:
+                                v = None
+                            if v is not None:
+                                values[match(category, strongly)] = v
+                                strongly = False
+                                category = None
+                            else:
+                                print(f'Skipping <{w}> (expected a number for {category} for Q{q})', \
+                                      file = stderr)    
+                    scores[q] = values
+                    # print(q, values)
+                    q += 1
+    return scores
+
+def textual(f):
+    reader = PdfReader(f)
+    content = ''
+    for page in reader.pages:
+        content += page.extract_text()
+    question = False
+    formulation = ''
+    term = None
+    year = None
+    instructor = None
+    lettercode = None
+    numbercode = None
+    section = None
+    prev = None
+    listing = []
+    for line in content.split('\n'):
+        line = line.strip().lstrip()
+        if len(line) == 0:
+            continue
+        if 'Mean' in line or 'Statistics' in line:
+            question = False
+            formulation = formulation.lstrip()
+            if len(formulation) > 0:
+                if formulation not in listing:
+                    listing.append(formulation)
+            formulation = ''
+            continue
+        if (line[0] == 'Q' and 'Questionnaire' not in line) \
+           or 'In general' in line \
+           or (line[:4] == 'The ' and 'ratings' not in line):
+            question = True
+        if question:
+            if line not in formulation:
+                formulation += ' ' + line
+        if 'Mercury Course Evaluation' in line:
+            continue
+        if '(Fall' in line:
+            term = 'F'
+            year, instructor = parse(line)
+        elif '(Winter' in line:
+            term = 'W'
+            year, instructor = parse(line)
+        elif '(Summer' in line:
+            term = 'S'
+            year, instructor = parse(line)
+        if 'Course Name ' in line:
+            fields = line.split()
+            lettercode = fields[2]
+            numbercode = fields[3][:-1] # colon
+            section = fields[-3][:-1] # comma
+    return (term, year, lettercode, numbercode, section, instructor, listing)
+        
 def complete(filename):
     for question, data in scores[filename].items():
         total = data.get(0, None)
@@ -177,12 +199,23 @@ def output(labels, values, target):
         if data is not None:
             s = [ data.get(v, 0) for v in range(1, 6) ]
             s = [ v if v is not None else 0 for v in s ]
-            status = 'ok' if sum(s) == data.get(0, None) else 'incomplete' 
+            diff = sum(s) - data.get(0, 0) 
+            status = 'consistent' if diff == 0 else f'off by {diff}' 
             f = [ when, what, who, q, ' '.join([ str(i) for i in s]), status ]
             print(';'.join(f), file = target) # semicolon separated file
-    
-for filename in dataset:
-    assert filename in scores
-    complete(filename)
-    with open('dataset.txt', 'w') as target:
-        output(dataset[filename], scores[filename], target)
+
+
+labels = {}
+scores = {}            
+for filename in os.listdir('.'):
+    if '.pdf' in filename:
+        f = os.path.join('.', filename)
+        if os.path.isfile(f):
+            print(f'Processing {filename[:30]}...', file = stderr)
+            scores[filename] = diagrams(f)
+            labels[filename] = textual(f)
+
+with open('dataset.txt', 'w') as target:
+    for filename in labels:
+        complete(filename)
+        output(labels[filename], scores[filename], target)
